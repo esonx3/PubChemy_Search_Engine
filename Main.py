924ef5b0a7e8cbae9a6c4662fde480fe8c51d4e4
esonx3 <tony.eson@hotmail.com>
2015-03-30 20:34:49 +0200

    added test function

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
 #Main file
+
+from pubchempy import *
+
+#Test function, remove later on
+# TODO Remove this later on
+def TestFunction(id):
+    comp = Compound.from_cid(id)
+    return comp.isomeric_smiles
+
+#print given value
+def PrintValue(val):
+    print val
+
+
+
+PrintValue(TestFunction(1423))
