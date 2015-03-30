a1e68d60e3814dfba93fb83548eac3b918d89b1f
esonx3 <tony.eson@hotmail.com>
2015-03-30 21:39:48 +0200

    added test function

--- Main.py
+++ Main.py
@@ -1,17 +1,34 @@
 #Main file
-
 from pubchempy import *
 
-#Test function, remove later on
-# TODO Remove this later on
-def TestFunction(id):
+#pubchempy doc: http://pubchempy.readthedocs.org/en/latest/
+
+
+#Retrieve Compound record for specified CID
+#in: CID
+#out: Compound record
+def GetCompound(id):
     comp = Compound.from_cid(id)
     return comp.isomeric_smiles
 
-#print given value
+#print given value or values, most likely not needed
+#in: item to print
 def PrintValue(val):
-    print val
+    try:
+        for obj in val:
+            print "item",val.index(obj)+1,":",obj
+    except:
+        print "one item:",val
 
+#Test function, remove later on
+# TODO Remove this later on
+def Test():
+    CID_list = [1423,1451,1321,25]
+    forms = list()
+    for obj in CID_list:
+        comp = GetCompound(obj)
+        forms.append(comp)
+    return forms
 
-
-PrintValue(TestFunction(1423))
+#print the returned values from the test function
+PrintValue(Test())
