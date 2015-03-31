c075d0f4855d39ba3c0e0d5f9981b7d3f645ac15
esonx3 <tony.eson@hotmail.com>
2015-03-31 20:00:06 +0200

    Updated Test script #3, now cheking for all pid's

--- Tests.py
+++ Tests.py
@@ -28,34 +28,41 @@ def DownloadDB(start,end):
                     mincount = 0
                     line = ""
                     insmen = ins + 500
-                    while continioue:
-                        if htmlAsString[ins] != ' ':
-                            #print htmlAsString[ins]
-                            if (strofcount == 7 and polcount == 2):
-                                line += htmlAsString[ins]
-                            if htmlAsString[ins] == '"':
-                                strofcount += 1
-                            elif htmlAsString[ins] == ':':
-                                polcount += 1
-                            elif htmlAsString[ins] == '-':
-                                mincount += 1
-                            Object.append(htmlAsString[ins])
-                            if (strofcount == 8 and polcount == 2 and mincount == 2):
-                                continioue = False
-                                htmlAsString = htmlAsString[ins:]
-                                print "found CAS: ", line, "at cid: ", current
+                    try:
+                        while continioue:
+                            if htmlAsString[ins] != ' ':
+                                #print htmlAsString[ins]
+                                if (strofcount == 7 and polcount == 2):
+                                    line += htmlAsString[ins]
+                                if htmlAsString[ins] == '"':
+                                    strofcount += 1
+                                elif htmlAsString[ins] == ':':
+                                    polcount += 1
+                                elif htmlAsString[ins] == '-':
+                                    mincount += 1
+                                Object.append(htmlAsString[ins])
+                                if (strofcount == 8 and polcount == 2 and mincount == 2):
+                                    continioue = False
+                                    htmlAsString = htmlAsString[ins:]
+                                    print "found CAS: ", line, "at cid: ", current
+                                    break;
+                            ins += 1
+                            if ins > insmen:
                                 break;
+                    except:
+                        print "logic error in loops, or index out of bounds....(stage 1)"
                         ins += 1
-                        if ins > insmen:
-                            break;
-                    if line[len(line)-1] == '"':
-                        line = line[:len(line)-1]
-                    tempList.append(line)
+                    try:
+                        if line[len(line)-1] == '"':
+                            line = line[:len(line)-1]
+                        tempList.append(line)
+                    except:
+                        print "missing an definition..."
                 #ListOfFoundObjects.append((line,current))
                 dicti[str(current)] = tempList
                 current += 1
             except:
-                print "logic error in loops, or index out of bounds...."
+                print "logic error in loops, or index out of bounds....(stage 2)"
                 current += 1
         except:
             print "WARNING! Object ",current, " passed!"
@@ -70,6 +77,43 @@ def openandprint():
     dicti = pickle.load(open( "CAS_Cid.txt", "rb" ))
     for keys,values in dicti.items():
         print keys,":",values
-
-DownloadDB(1,5)
+def Cleaner():
+    dicti = pickle.load(open( "CAS_Cid.txt", "rb" ))
+    for keys,values in dicti.items():
+        try:
+            if values[0] == 'RecordNumber' or values[0] == ' ' :
+              del dicti[keys]
+        except:
+            pass
+    pickle.dump(dicti,open( "CAS_Cid.txt", "wb" ))
+DownloadDB(1,250)
+Cleaner()
+#print "Next range.."
+#DownloadDB(250,500)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(500,750)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(750,1000)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(1000,1250)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(1250,1500)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(1500,1750)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(1750,2000)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(2250,2500)
+#Cleaner()
+#print "Next range.."
+#DownloadDB(2500,3000)
+#Cleaner()
+#print "Done!"
 openandprint()
