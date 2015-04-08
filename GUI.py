25450cc3f053d5d1ff4356d1029bc3c2469bd90c
Alexander Karlsson <ikolito@gmail.com>
2015-04-08 14:00:19 +0200

    e.focus() Return key is not working the same way as "ok"-button

--- GUI.py
+++ GUI.py
@@ -20,13 +20,27 @@ def onKey(event):
 def hello():
     print "hello!"
 
-
 def about():
-    print "Ange Namn, Cas eller Smiley som du vill leta efter"
+    top = Toplevel()
+    top.title("Python PubChem App")
+
+    msg = Message(top, text="PubChem Client")
+    msg.pack()
+
+    button = Button(top, text="Ok", command=top.destroy)
+    button.pack()
+
+def open_file(self):
+    fname = askopenfilename(filetypes=(("Template files", "*.tplate"),
+                                           ("HTML files", "*.html;*.htm"),
+                                           ("All files", "*.*") ))
 
 def search(event):
-    print B.cget("state")
-    print "ressultat in progress..."
+    print E.get()
+    if B.cget("state"):
+        print B.cget("state")
+    else:
+        print "Disabled!"
 
 def GO_name(name):
     sometext=str(E.get())
@@ -42,19 +56,22 @@ C = Canvas(root, bg="white", bd=4, width=355, height=300, relief=GROOVE)
 C.pack()
 v = StringVar()
 
-E = Entry(frame,textvariable=v)
-E.grid(row=1, column=1)
-E.bind('<KeyRelease>',onKey)
-
-#create Button
+# Button
 B=Button(frame,state='disabled',command=lambda: GO_name(E),text="GO!",fg="blue",bg="red",width=5)
 B.bind('<Button-1>', search)
 B.grid(row=1, column=2)
 
+# Entry widget
+E = Entry(frame,textvariable=v)
+E.grid(row=1, column=1)
+E.bind('<Return>',search)
+E.bind('<KeyRelease>',onKey)
+E.focus()
+
 
 #create OptionMenu
 var2 = StringVar(root)
-var2.set("Search Type") # initial value
+var2.set("Name") # initial value
 option = OptionMenu(frame, var2, "Name", "Smiley", "CAS")
 option.grid(row=1, column=0)
 
@@ -63,14 +80,14 @@ menubar = Menu(root)
 
 # create pulldown menus, and add them to the menu bar
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command=hello) #change command to "read file"
+filemenu.add_command(label="Open", command=open_file) #change command to "read file"
 filemenu.add_command(label="Save", command=hello) #change command to "save file"
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 historymenu = Menu(menubar, tearoff=0)
-historymenu.add_command(label="Latest Serch", command=hello)
+historymenu.add_command(label="Latest Search", command=hello)
 historymenu.add_command(label="Random function", command=hello)
 menubar.add_cascade(label="History", menu=historymenu)
 
